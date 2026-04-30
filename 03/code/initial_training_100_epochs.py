6e1b4025cb8e015d874dc584f077644f26c96829
neeharikajasti <neeharikajasti@gmail.com>
2026-04-30 14:18:55 +0530

    Update initial_training_100_epochs.py

--- 03/code/initial_training_100_epochs.py
+++ 03/code/initial_training_100_epochs.py
@@ -1,16 +1,18 @@
 import os
 from ultralytics import YOLO
 
-BASE_DIR = os.getcwd()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-data_path = os.path.join(BASE_DIR, 'code', 'data.yaml')
+data_path = os.path.join(BASE_DIR, 'data.yaml')
 
-model = YOLO('yolov8n.pt') 
+model = YOLO('yolov8n.pt')
 
 results = model.train(
     data=data_path,
     epochs=100,
     imgsz=640,
     workers=6,
-    batch=16
+    batch=16,
+    project=os.path.join(BASE_DIR, '..', 'runs'),
+    name='insulator_model'
 )
