2efc31270d5720af04210bd608a2479e574ccb37
kshirja <54768266+kshirja@users.noreply.github.com>
2026-04-30 15:59:40 +0530

    Add files via upload

--- 05/code/train.py
+++ 05/code/train.py
@@ -1,1 +1,11 @@
-# pothole detection training script
+from ultralytics import YOLO
+
+# load model
+model = YOLO("yolov8n.pt")  # lightweight model
+
+# train model
+model.train(
+    data="data/data.yaml",
+    epochs=10,
+    imgsz=640
+)
